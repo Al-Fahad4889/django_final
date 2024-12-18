102a339db70f4407571256779278c85c89bdaaab
Abdur65 <103262810+Abdur65@users.noreply.github.com>
2024-12-18 18:18:46 +0600

    Update user models.py

--- EventPlanner/users/models.py
+++ EventPlanner/users/models.py
@@ -1,38 +1,21 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, name, password=None, **extra_fields):
-        if not email:
-            raise ValueError("The Email field must be set")
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, name, password=None, **extra_fields):
-        extra_fields.setdefault('is_admin', True)
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, name, password, **extra_fields)
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    user_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)
-    is_attendee = models.BooleanField(default=False)
-    is_organizer = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
+class User(models.Model):
+    # Roles of users
+    ROLE_CHOICES = [
+        ('organizer', 'Organizer'),
+        ('attendee', 'Attendee'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='attendee')  # Role assigned to the user
     
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
-
-    def __str__(self):
-        return self.email
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, null=True) # short description of user
+    location = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    social_links = models.JSONField(default=dict, blank=True)  # Store links to social accounts
+    preferences = models.JSONField(default=dict, blank=True)  # Store user preferences
