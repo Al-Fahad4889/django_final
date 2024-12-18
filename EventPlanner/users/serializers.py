1de9e76e044e6ca5089237f9152a6541286ab6e7
Abdur65 <103262810+Abdur65@users.noreply.github.com>
2024-12-18 22:37:50 +0600

    Updated serializers.py

--- EventPlanner/users/serializers.py
+++ EventPlanner/users/serializers.py
@@ -1,12 +1,41 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import User, Profile
 
+# User serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'is_attendee', 'is_organizer', 'is_admin']
+        fields = ['id', 'username', 'email', 'role']
 
+
+# Profile serializer
 class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only = True)
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'bio', 'location', 'phone']
+        fields = '__all__' 
+
+
+# Registration serializers 
+class UserRegistrationSerializers(serializers.ModelSerializer):
+    password = serializers.CharField(write_only = True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role']
+        
+        def create(self, validated_data):
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password'],
+                role=validated_data['role']
+            )
+            Profile.objects.create(user=user)  # Automatically create a profile
+            return user
+    
+# Login serializer
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
