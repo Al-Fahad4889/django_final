29a133d2755830b13846318c834825ef824e51a4
Abdur65 <103262810+Abdur65@users.noreply.github.com>
2024-12-19 23:21:49 +0600

    Updated serializers.py for events

--- EventPlanner/feedback/serializers.py
+++ EventPlanner/feedback/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from .models import Feedback
+from .models import Event, User
 
-class FeedbackSerializer(serializers.ModelSerializer):
+class EventUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Feedback
-        fields = ['id', 'event', 'user', 'rating', 'comment']
+        model = User
+        fields = ('username',)
+        
+class EventSerializer(serializers.ModelSerializer):
+    organizer_username = serializers.CharField(source='organizer.username', read_only=True)
+
+    class Meta:
+        model = Event
+        fields = ('id', 'organizer_username', 'name', 'description', 'date', 'venue', 'price', 'created_at', 'updated_at', 'status')
+
+class EventDetailSerializer(serializers.ModelSerializer):
+  organizer = EventUserSerializer(read_only=True)
+
+  class Meta:
+    model = Event
+    fields = ('id', 'organizer', 'name', 'description', 'date', 'venue', 'price', 'created_at', 'updated_at', 'status')
+
+class EventStatusUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ('status',)
+
