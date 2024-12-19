c864b969a2ed82e38cc22dd40f16f277cd2706d0
Al-Fahad4889 <alfahad4889@gmail.com>
2024-12-20 00:22:47 +0600

    deleted files

--- EventPlanner/events/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('events/', views.EventListView.as_view(), name='event-list'),
-    path('events/<int:event_id>/', views.EventDetailView.as_view(), name='event-detail'),
-    path('events/<int:event_id>/approve/', views.EventApproveView.as_view(), name='event-approve'),
-]
--- EventPlanner/feedback/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('feedback/', views.FeedbackCreateView.as_view(), name='feedback-create'),
-    path('feedback/event/<int:event_id>/', views.FeedbackEventView.as_view(), name='feedback-event'),
-    path('feedback/user/<int:user_id>/', views.FeedbackUserView.as_view(), name='feedback-user'),
-]
