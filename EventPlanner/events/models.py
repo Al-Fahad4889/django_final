48bc378207ef455b102fab5005d98eadce164c1b
Abdur65 <103262810+Abdur65@users.noreply.github.com>
2024-12-19 23:23:58 +0600

    Updated models.py for events

--- EventPlanner/events/models.py
+++ EventPlanner/events/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Event(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    )
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organized_events')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    venue = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    
