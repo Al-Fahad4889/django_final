521d63bf97a449ce8970851552f1a228815a9382
Al-Fahad4889 <alfahad4889@gmail.com>
2024-12-20 02:27:05 +0600

    before fixing problems with point break's code

--- EventPlanner/settings.py
+++ EventPlanner/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
+    'rest_framework',
+    'users',
+    'events',
+    'notifications',
+    'tickets',
+    'transactions',
+    'feedback',
 ]
 
 MIDDLEWARE = [
--- EventPlanner/urls.py
+++ EventPlanner/urls.py
@@ -15,14 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 from django.contrib import admin
 from django.urls import path, include
-
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API documentation for the Event Planner project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="your_email@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
@@ -31,4 +44,8 @@ urlpatterns = [
     path('api/transactions/', include('transactions.urls')),
     path('api/notifications/', include('notifications.urls')),
     path('api/tickets/', include('tickets.urls')),  # Add tickets URLs
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
